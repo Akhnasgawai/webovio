bdc82ba17ea9978c09381230192ad238a0614b62
Mohammed Akhnas Gawai <akhnasg@gmail.com>
2023-04-28 17:36:47 +0530

    animations added

--- js/main.js
+++ js/main.js
@@ -21,3 +21,28 @@ mediaQuery2.addEventListener('change', function () {
         }
     }
 })
+
+const translationObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('translation-show');
+        } else {
+            entry.target.classList.remove('translation-show');
+        }
+    });
+});
+
+const scaleObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('scale-show');
+        } else {
+            entry.target.classList.remove('scale-show');
+        }
+    });
+});
+
+const translationElements = document.querySelectorAll(".translation-animation");
+const scaleElements = document.querySelectorAll(".scale-animation");
+translationElements.forEach((el) => translationObserver.observe(el));
+scaleElements.forEach((el) => scaleObserver.observe(el));
